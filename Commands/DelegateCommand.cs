d68525061526868b098757353b30b88c51786c08
Aadarsh Tiwari <117271222+aadarsh0001@users.noreply.github.com>
2022-11-29 13:44:58 +0530

    Add files via upload

---
+++ Commands/DelegateCommand.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MyRetailStore.Commands
+{
+    public class DelegateCommand<T> : System.Windows.Input.ICommand where T : class
+    {
+        private readonly Predicate<T> _canExecute;
+        private readonly Action<T> _execute;
+
+        public DelegateCommand(Action<T> execute)
+            : this(execute, null)
+        {
+        }
+
+        public DelegateCommand(Action<T> execute, Predicate<T> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecute == null)
+                return true;
+
+            return _canExecute((T)parameter);
+        }
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CommandManager.InvalidateRequerySuggested();
+        }
+        public void Execute(object parameter)
+        {
+            _execute((T)parameter);
+        }
+
+    }
+}
---
+++ Commands/RelayCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MyRetailStore.Commands
+{
+    class RelayCommand:ICommand
+    {
+        public RelayCommand(Action<object> action)
+        {
+            mAction = action;
+        }
+        private Action<object> mAction;
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (parameter != null)
+            {
+                mAction(parameter);
+            }
+            else
+            {
+                mAction(System.Windows.MessageBox.Show("The Parameter does not have any value", "Error-Parameter value", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Information));
+            }
+        }
+    }
+}
