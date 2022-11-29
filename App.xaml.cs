56291c64eac7b0496661883d4d3570231b7c5281
Aadarsh Tiwari <117271222+aadarsh0001@users.noreply.github.com>
2022-11-29 13:34:09 +0530

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MyRetailStore
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static string UserId { get; internal set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            Window loginWindow = new Views.LoginView();
+            loginWindow.DataContext = new ViewModels.LoginViewModel();
+            loginWindow.Show();
+
+            
+
+            //if (((ViewModels.LoginViewModel)loginWindow.DataContext).IsLoginSuccessfull == true)
+            //{
+            //    loginWindow.Hide();
+            //    Window mainwindow = new Views.MainView();
+            //    mainwindow.DataContext = new ViewModels.MainViewModel();
+            //    mainwindow.Show();
+            //    loginWindow.Close();
+            //}
+
+
+            
+        }
+    }
+}
